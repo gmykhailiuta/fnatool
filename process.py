d4484fe4cf0889f95851fe6a36a8526930ee0049
Gennadiy Mykhailiuta <hmihailiuta@cogniance.com>
2013-03-04 18:15:17 +0200

    Made code to be usable as a module. Bugfixes

--- process.py
+++ process.py
@@ -8,13 +8,10 @@ from math import atan, log, pi
 from scipy import signal
 import anfft
 
-record = 's20011'
-records = []
-
-def write_csv():
+def write_csv(records,record):
 	outfile = open(record+".csv", "w")
 	for r in records:
-		line = "%s,%s,%s,%s,%s,%s\n" % record,r['time_from'],r['time_to'],r['std'],r['stn'],r['mean']
+		line = "%s,%s,%s,%s,%s,%s\n" % (record,r['time_from'],r['time_to'],r['std'],r['stn'],r['mean'])
 	outfile.write(line)
 	outfile.close()
 
@@ -30,13 +27,13 @@ def autocor(data):
 	cor = cor[len(cor)/2:]
 	return cor
 
-def fft(x,width=20):
+def fft(x,samp_freq=250,width=20):
 	print "FFT"
 	arr = numpy.zeros(2**width)
 	arr[:len(x)] = x
 	F = abs(anfft.fft(arr))
 #	F = abs(pylab.fftpack.fft(arr))
-	f = pylab.fftfreq(len(arr), 1/info['samp_freq'])
+	f = pylab.fftfreq(len(arr), 1/samp_freq)
 	F = F[:len(F)/2]
 	f = f[:len(f)/2]
 	return F, f
@@ -65,64 +62,69 @@ def fig(in1,in2=None,label="",show=False):
 		pylab.show()
 		exit(0)
 
-# Read in the data from 0 to 10 seconds
-# rdsamp(record, start=0, end=-1, interval=-1)
-data, info = rdsamp(record, 0)
-pprint(info)
+def process(record, write_file=True):
+	records = []
 
-print "total time ", int(info['samp_count'])/int(info['samp_freq'])
+	# Read in the data from 0 to 10 seconds
+	# rdsamp(record, start=0, end=-1, interval=-1)
+	data, info = rdsamp(record, 0)
+	pprint(info)
+
+	print "total time ", int(info['samp_count'])/int(info['samp_freq'])
 
 # rdann(record, annotator, start=0, end=-1, types=[])
-ann = rdann(record, 'atr', 0)
+	ann = rdann(record, 'atr', 0)
 # annotation time in samples from start
-ann_x = (ann[:, 0] - data[0, 0]).astype('int')
+	ann_x = (ann[:, 0] - data[0, 0]).astype('int')
 
 #ann_y = v[ann_y,2]
 #plot_data(data, info, ann)
 
-c = 0 # window first RR
-w = 1000 # window last RR
+	c = 0 # window first RR
+	w = 1000 # window last RR
 
-print "RR count", len(ann)
+	print "RR count", len(ann)
 
-while c+w < len(ann):
-	r = {}
+	while c+w < len(ann):
+		r = {}
 	# get 10 RRs
-	print "RR ",c," - ",c+w
-	chunk_first = ann[c][0] # get sample number of first RR interval
-	chunk_last = ann[c+w][0] # get sample number of last RR interval
+		print "RR ",c," - ",c+w
+		chunk_first = ann[c][0] # get sample number of first RR interval
+		chunk_last = ann[c+w][0] # get sample number of last RR interval
 	#time_interval = (chunk_last-chunk_first)/info['samp_freq']
-	r['time_from'] = data[chunk_first,1]
-	r['time_to'] = data[chunk_last,1]
-	print "interval ", data[chunk_first,1], " - ", data[chunk_last,1], " = ", (chunk_last-chunk_first)/info['samp_freq'], " s"
+		r['time_from'] = data[chunk_first,1]
+		r['time_to'] = data[chunk_last,1]
+		print "interval ", r['time_from'], " - ", r['time_to'], " = ", (chunk_last-chunk_first)/info['samp_freq'], " s"
 
-	t = data[chunk_first:chunk_last,1] # time array of target window
-	v = data[chunk_first:chunk_last,2] # data value of target window
+		t = data[chunk_first:chunk_last,1] # time array of target window
+		v = data[chunk_first:chunk_last,2] # data value of target window
 
-	#fig(t,v,show=True)
-	r['std'] = pylab.std(v)
-	r['mean'] = pylab.mean(v)
-	r['stn'] = r['std']/r['mean']
+#		fig(t,v)
+		r['std'] = pylab.std(v)
+		r['mean'] = pylab.mean(v)
+		r['stn'] = r['std']/r['mean']
+		
+#		import pywt
+#		cA, cD = pywt.dwt(v, 'coif3')
+#		fig(cA)
+#		fig(cD, show=True)
 
-	cor = autocor(v)
+		cor = autocor(v)
 
+		Ff, ff = fft(cor, info['samp_freq'])
+		Fc, fc = cut_freq(Ff,ff)
 
-
-	Ff, ff = fft(cor)
-	Fc, fc = cut_freq(Ff,ff)
-
-	Fc = pylab.log(Fc)
-	fc = pylab.log(fc)
+		Fc = pylab.log(Fc)
+		fc = pylab.log(fc)
 #	fig(fc,Fc,show=True)
 
-	r['betta'] = betta(fc,Fc)
+		r['betta'] = betta(fc,Fc)
 	
-	pprint(r)
+		pprint(r)
+		records.append(r)
+		c +=w
 
-	records.append(r)
+	write_csv(records,record)
 
-	c +=w 
-
-
-write_csv()
-
+if __name__ == '__main__':
+	process('s20011')
