52ee446866198fd6073141ecf6d499b8689e9440
Gennadiy Mykhailiuta <hmihailiuta@cogniance.com>
2013-05-22 19:57:32 +0300

    Cleanup & review

--- process.py
+++ process.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 import pylab as pl
-from wfdbtools import rdhdr, rdann, plot_data
+from wfdbtools import rdhdr, rdann
 from sys import exit
 import datetime as dt
 from scipy.interpolate import interp1d,splrep,splev
 from pprint import pprint
 
 WINDOW = 4096 # should be 2**x
-INTERP_FREQ = 8 # should be 2**x
-INTERP_KIND = 'cubic' # linear/cubic
+INTERP_FREQ = 4 # should be 2**x
+INTERP_KIND = 'linear' # linear/cubic
 FREQ_LIMIT = (0.0033, 0.4) # (min_freq, max_freq)
-BETA_LIMIT = (0.5, 1.5) # used to be 0.5 < beta < 1.5
+BETA_LIMIT = (0.2, 1.8) # used to be 0.5 < beta < 1.5
 VALID_RR_RATIO = 0.2 # 0 < x < 1
 
 
@@ -89,8 +89,8 @@ def fft_filter(time, signal, result, samp_freq=INTERP_FREQ, freq_limit = FREQ_LI
   #signal_filt_uwed = signal_filt_uwed[:len(time)]
   #signal = signal[:len(time)]
  
-  spec_abs = pl.absolute(spec)[n/2:n] # half of absolute spectra for orig signal
-  spec_filt_abs = pl.absolute(spec_filt)[n/2:n] # half of absolute spectra for filt signal
+  spec_abs = pl.absolute(spec)[n/2:n]*2/n # half of absolute spectra for orig signal
+  spec_filt_abs = pl.absolute(spec_filt)[n/2:n]*2/n # half of absolute spectra for filt signal
   freq_abs = freq[n/2:n] # half of absolute freqs axis for orig signal
   freq_filt_abs = freq_filt[n/2:n] # half of absolute freqs axis for filt signal
 
@@ -99,8 +99,8 @@ def fft_filter(time, signal, result, samp_freq=INTERP_FREQ, freq_limit = FREQ_LI
   freq_filt_abs = pl.ma.masked_equal(freq_filt_abs,0)
   freq_filt_abs = pl.ma.compressed(freq_filt_abs)
 
-  spec_filt_abs *= 2 # we cut off half of spectra - needs to be compensated
-  spec_abs *= 2
+  #spec_filt_abs *= 2/n # we cut off half of spectra - needs to be compensated
+  #spec_abs *= 2/n
 
   spec_filt_log = 20*pl.log10(spec_filt_abs) # for output
   freq_filt_log = 20*pl.log10(freq_filt_abs)
@@ -209,7 +209,7 @@ def bandPass_(T, V, fname):
   pl.plot(ffs,F)
 
   for i in range(n):  # frequency cutting
-    if 0.0033 < abs(ffs[i]) and abs(ffs[i]) < 0.04:
+    if 0.0033 < abs(ffs[i]) and abs(ffs[i]) < 0.4:
     #if abs(ffs[i]) < 0.0033:
     #if abs(ffs[i]) < 0.4:
       P[i] = I[i]
@@ -307,24 +307,27 @@ def draw(in1,in2=None,label="",show=False):
     exit(0)
 
 
-def plot_beta_cv(results):
+def plot_beta_cv(results, preview=False):
   x = [x['cov'] for x in results]
   y = [y['beta'] for y in results]
   pl.figure(figsize=(6, 6), facecolor='white')
+  pl.title(r'%(record)s: $\sigma/\bar{x}$' % results[0])
   pl.plot(x,y,'.r')
   pl.axhline(y=1,color='b')
   pl.ylim(0,2)
   pl.xlabel(r"$\sigma/\bar{x}$")
   pl.ylabel(r"$\beta$")
   pl.savefig("cv_%(record)s.png" % results[0],facecolor='w',edgecolor='k',transparent=True)
+  if preview:  
+    pl.show()
   pl.close()
 
 
-def plot_beta_std(results):
+def plot_beta_std(results, preview=False):
   x = [x['std'] for x in results]
   y = [y['beta'] for y in results]
   pl.figure(figsize=(6, 6), facecolor='white')
-  pl.title(r'%(record)s: $\beta/\sigma$' % results)
+  pl.title(r'%(record)s: $\beta/\sigma$' % results[0])
   pl.plot(x,y,'.r')
   pl.ylim(0,2)
   pl.xlim(0,140)
@@ -333,11 +336,12 @@ def plot_beta_std(results):
   pl.xlabel(r"$\sigma$, ms")
   pl.ylabel(r"$\beta$")
   pl.savefig("std_%(record)s.png" % results[0],facecolor='w',edgecolor='k',transparent=True)
-  #pl.show()
+  if preview:
+    pl.show()
   pl.close()
 
 
-def plot_beta(freq, fft, aprox_y, result):
+def plot_beta(freq, fft, aprox_y, result, preview=False):
   pl.figure(figsize=(6, 6),facecolor='white')
   pl.plot(freq,fft,'b')
   pl.plot(freq,aprox_y,'r')
@@ -346,7 +350,8 @@ def plot_beta(freq, fft, aprox_y, result):
   pl.xlabel(r"$\lg f$")
   pl.ylabel(r"$\lg S$")
   pl.savefig("beta_%(record)s_%(frag)s.png" % result,facecolor='w',edgecolor='k',transparent=True)
-  #pl.show()
+  if preview and result['frag'] == 1:
+    pl.show()
   pl.close()
 
 
@@ -375,22 +380,25 @@ def variability(r_times, valid_rr_ratio=VALID_RR_RATIO):
   last_rr = 0
   time[0] = 0
   for i in range(1, len(r_times)):
-    rr = r_times[i]-r_times[i-1]
+    rr = (r_times[i]-r_times[i-1])* 1000
     if last_rr: # if first rr computed, go with validation
-      if abs(last_rr / rr - 1) <= valid_rr_ratio: # current rr differs less then 20% of previous one
+      if (abs(last_rr / rr - 1) <= valid_rr_ratio) and (rr > 100) and (rr < 2000): # current rr differs less then 20% of previous one
         rrs[i] = rr
+        last_rr = rr
       else:
         rrs[i] = 0
         time[i] = 0
     else: # no rrs computed yet. need first for validation to work
       rrs[i] = rr
-    last_rr = rr
+      last_rr = rr
+    #last_rr = rr
+
 
   rrs = pl.ma.masked_equal(rrs,0)
   rrs = pl.ma.compressed(rrs)
   time = pl.ma.masked_equal(time,0)
   time = pl.ma.compressed(time)
-  rrs *= 1000
+  #rrs *= 1000
   print "NB: Deleted %0.2f%% intervals" % (float(len(r_times)-len(rrs))/len(r_times)*100,)
   return time, rrs
 
@@ -445,11 +453,11 @@ def plot_signals(info, hrv_time, hrv_data, hrv_interp_time, hrv_interp_data, ann
     #pl.plot(hrv_time, ann[:, 1], 'xr')
     #pl.ylabel('RR') 
 
-    sp1 = pl.subplot(211)
+    #sp1 = pl.subplot(211)
     pl.plot(hrv_time, hrv_data, 'x')
     #pl.xlim(sp1.get_xlim()) 
     pl.ylabel('HRV (s)') 
-    pl.subplot(212)
+    #pl.subplot(212)
     pl.plot(hrv_interp_time, hrv_interp_data, 'k',ls='dotted')
     pl.xlabel('Time (s)')
     #sp1.set_xlim(0,1000)
@@ -478,7 +486,7 @@ def interpolate(x, y, kind='linear'):
     return xnew, ynew
 
 
-def process_part(record, annotator, diagnosis=None, start=0, end=-1, beta_limit=BETA_LIMIT):
+def process_part(record, annotator, diagnosis=None, start=0, end=-1, beta_limit=BETA_LIMIT, preview=False):
   # Read in the data from 0 to 10 seconds
   # rdsamp(record, start=0, end=-1, interval=-1)
   info = rdhdr(record)
@@ -491,11 +499,14 @@ def process_part(record, annotator, diagnosis=None, start=0, end=-1, beta_limit=
   # annotation time in samples from start
   #ann_x = (ann[:, 0] - data[0, 0]).astype('int')
 
-  time, hrv = variability(ann[:,1])
-  draw(time)
+  
+  time, hrv = variability(ann[:,1]-ann[0,1])
+  #draw(time,hrv, show=True)
+  #exit()
   time_interp, hrv_interp = interpolate(time, hrv, INTERP_KIND)
   #time_interp, hrv_interp = (time, hrv)
-  plot_signals(info, time, hrv, time_interp, hrv_interp, ann)
+  if preview:
+    plot_signals(info, time, hrv, time_interp, hrv_interp, ann)
   #exit(0)
 
   #signal_to_csv(record, time, hrv, info)
@@ -541,7 +552,7 @@ def process_part(record, annotator, diagnosis=None, start=0, end=-1, beta_limit=
     _a,_b = approximate(freq_filt,fft_filt)
     result['beta'] = -_a
     line_y = [_a*x+_b for x in freq_filt]
-    plot_beta(freq_filt,fft_filt,line_y,result)
+    plot_beta(freq_filt, fft_filt, line_y, result, preview=preview)
 
     if beta_limit[0] <= result['beta'] and result['beta'] <= beta_limit[1]:
       results.append(result)
@@ -569,42 +580,36 @@ def stats(results):
   print "Median:\t\t\t\t%(beta)0.2f\t%(std)d\t%(cov)0.2f%%\t%(mean)d" % medians
   print "Stdev:\t\t\t\t%(beta)0.2f\t%(std)d\t%(cov)0.2f%%\t%(mean)d" % stdevs
 
-def process_signal(ann_file_name, diagnosis=None):
+def process_signal(ann_file_name, diagnosis=None, preview=False):
     (record, annotator) = ann_file_name.split('.')
-    info, results = process_part(record, annotator, diagnosis)
+    info, results = process_part(record, annotator, diagnosis, preview=preview)
     if not results:
       print "No results"
       return
     stats(results)
-    results_to_csv(results, record, info)
-    #plot_beta_std(results, "std_%s.png" % (record, ))
-    #plot_beta_cv(results, "cv_%s.png" % (record, ))
+    results_to_csv(results, record, info, preview=preview)
+    plot_beta_std(results, preview=preview)
+    plot_beta_cv(results, preview=preview)
 
 if __name__ == '__main__':
-  #signals = ['16273 atr', '16272 atr',  'chf06 ecg', 'chf07 ecg', '16420 atr', 's20051 atr']
-  #signals = ['s20051 atr', 's20411 atr']
-  #signals = ['16265.atr  16272.atr  16273.atr  16420.atr  16483.atr  16539.atr  16773.atr  16786.atr  16795.atr']
-  #signals = ['chf03 ecg', 'chf04 ecg']
+  signals = {
+    'Normal': '16265.atr  16273.atr  16483.atr  16773.atr  16795.atr  17453.atr  18184.atr  19090.atr  19140.atr  16272.atr  16420.atr  16539.atr  16786.atr  17052.atr  18177.atr  19088.atr  19093.atr  19830.atr',
+    'CHF': 'chf01.ecg  chf03.ecg  chf05.ecg  chf07.ecg  chf09.ecg  chf11.ecg  chf13.ecg  chf15.ecg  chf02.ecg  chf04.ecg  chf06.ecg  chf08.ecg  chf10.ecg  chf12.ecg  chf14.ecg',
+    'Hypertension': 's20031.atr  s20121.atr  s20221.atr  s20471.atr  s20551.atr  s20651.atr  s30691.atr  s30751.atr  s30791.atr  s20051.atr  s20131.atr  s20341.atr  s20481.atr  s20561.atr  s30661.atr  s30741.atr  s30752.atr  s30801.atr  s20101.atr  s20171.atr  s20411.atr  s20501.atr  s20581.atr  s30681.atr  s30742.atr  s30761.atr'
+    }
   batch = False
   if batch:
-    #health = '16265.atr  16273.atr  16483.atr  16773.atr  16795.atr  17453.atr  18184.atr  19090.atr  19140.atr  16272.atr  16420.atr  16539.atr  16786.atr  17052.atr  18177.atr  19088.atr  19093.atr  19830.atr'
-    #for signal in health.split('  '):
-    #  process_signal(signal, 'No deseases')
-    #hypert = 's20031.atr  s20121.atr  s20221.atr  s20471.atr  s20551.atr  s20651.atr  s30691.atr  s30751.atr  s30791.atr  s20051.atr  s20131.atr  s20341.atr  s20481.atr  s20561.atr  s30661.atr  s30741.atr  s30752.atr  s30801.atr  s20101.atr  s20171.atr  s20411.atr  s20501.atr  s20581.atr  s30681.atr  s30742.atr  s30761.atr'
-    hypert = 's20471.atr  s20551.atr  s20651.atr  s30691.atr  s30751.atr  s30791.atr  s20051.atr  s20131.atr  s20341.atr  s20481.atr  s20561.atr  s30661.atr  s30741.atr  s30752.atr  s30801.atr  s20101.atr  s20171.atr  s20411.atr  s20501.atr  s20581.atr  s30681.atr  s30742.atr  s30761.atr'
-    for signal in hypert.split('  '):
-     process_signal(signal, "Hypetension")
-    chf = 'chf01.ecg  chf03.ecg  chf05.ecg  chf07.ecg  chf09.ecg  chf11.ecg  chf13.ecg  chf15.ecg  chf02.ecg  chf04.ecg  chf06.ecg  chf08.ecg  chf10.ecg  chf12.ecg  chf14.ecg'
-    for signal in chf.split('  '):
-     process_signal(signal, "CHF")
+    diagnosis = 'Hypertension'
+    for record in signals[diagnosis].split('  '):
+      process_signal(record, diagnosis)
   else:
     #signals = ['16483.atr', '16773.atr']
     #signals = ['16273.atr', '16272.atr']
-    #signals = ['chf03.ecg', 'chf04.ecg']
-    #signals = ['chf05.ecg', 'chf06.ecg']
-    signals = ['nsr001.ecg']
+    #signals = ['chf04.ecg']
+    signals = ['chf05.ecg', 'chf06.ecg']
+    #signals = ['nsr004.ecg']
     for signal in signals:
-      process_signal(signal, "Normal")
+      process_signal(signal, "CHF", preview=True)
 
   
     
